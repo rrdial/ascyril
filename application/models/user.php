a86e38a5f0b23e77b31f58c10abe2e505307b3be
R Ryan Dial <rrdial@gmail.com>
2013-02-07 12:19:15 -0500

    Basic layout for computer details page. Static activity log for now. Gravatar support for User images.

--- application/models/user.php
+++ application/models/user.php
@@ -6,4 +6,20 @@ class User extends Eloquent {
     return $this->has_many('Comment');
   }
 
+  public function gravatar($size = 64) {
+    if (isset($this->email)) {
+      $url = 'http://www.gravatar.com/avatar/' . md5($this->email);
+    } else {
+      $url = 'http://www.gravatar.com/avatar/' . md5('noemailprovided');
+    }
+    if (!($size += 0))
+      $size = 64;
+    $options = array(
+        's' => $size,
+        'd' => 'wavatar',
+        'r' => 'g'
+    );
+    return $url . '?' . http_build_query($options);
+  }
+
 }
---
+++ application/views/computers/view.blade.php
@@ -0,0 +1,60 @@
+@layout('template')
+
+@section('title', 'Computer details')
+
+@section('content')
+<div class="page-header">
+  <h1>Computer {{$computer->asset_tag}}</h1>
+</div>
+
+<?php /*
+<dl>
+  <dt>Current status</dt>
+  <dd>{{$computer->status ? $computer->status->value : 'unavailable'}}</dd>
+  
+  <dt>Serial Number</dt>
+  <dd>{{$computer->sn}}</dd>
+  
+  <dt>ARD name</dt>
+  <dd>gates-student-{{$computer->name}}</dd>
+</dl>
+*/ ?>
+<div class="row">
+  <div class="span4">
+    <h4>Current Status</h4>
+    {{$computer->status ? $computer->status->value : 'unavailable'}}
+  </div>
+  <div class="span4">
+    <h4>Serial Number</h4>
+    {{$computer->sn}}
+  </div>
+  <div class="span4">
+    <h4>ARD Name</h4>
+    gates-student-{{intval($computer->name)}}
+  </div>
+</div>
+
+<h2 class="page-header">Activity Log</h2>
+<?php
+$user = new \User();
+$user->nickname = 'Ryan Dial';
+$log = new stdClass();
+$log->message = 'Smashed it with a sledge hammer.';
+$log->user = $user;
+
+$logs = array($log);
+?>
+<?php foreach ($logs as $log) : ?>
+  <div class="media">
+    <div class="pull-left">
+      <img class="media-object" src="{{$log->user->gravatar(64)}}">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">{{$log->user->nickname}}</h4>
+      {{$log->message}}
+    </div>
+  </div>
+<?php endforeach; ?>
+
+<?php //var_dump($computer); ?>
+@endsection
