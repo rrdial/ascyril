e8a00fc9c68e71714969b799052da709694dd43b
R Ryan Dial <rrdial@gmail.com>
2013-02-13 14:04:38 -0500

    New computers are created successfully.

--- application/controllers/computers.php
+++ application/controllers/computers.php
@@ -8,9 +8,25 @@ class Computers_Controller extends Base_Controller {
     return View::make('computers.index')
                     ->with('computers', Computer::all());
   }
-  
+
   public function post_index() {
-    var_dump(Input::all());
+    $input = array(
+        'asset_tag' => Input::get('asset_tag')
+    );
+    $rules = array(
+        'asset_tag' => 'required|integer|unique:computers'
+    );
+    $v = Validator::make($input, $rules);
+    if ($v->passes()) {
+      $c = new Computer();
+      $c->asset_tag = $input['asset_tag'] + 0;
+      $c->save();
+      return Redirect::to_action('computers@index', array('computer_inserted' => TRUE), 303);
+    } else {
+      return Redirect::to_action('computers@index', array(), 303)
+                      ->with_errors($v)
+                      ->with_input();
+    }
   }
 
 }
