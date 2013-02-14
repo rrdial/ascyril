d52faefb813a3fddec96aae82364f440e75b0ba1
R Ryan Dial <rrdial@gmail.com>
2013-02-14 15:00:48 -0500

    Get to the edit screen right away

--- application/controllers/computers.php
+++ application/controllers/computers.php
@@ -21,7 +21,8 @@ class Computers_Controller extends Base_Controller {
       $c = new Computer();
       $c->asset_tag = $input['asset_tag'] + 0;
       $c->save();
-      return Redirect::to_action('computers@index', array('computer_inserted' => TRUE), 303);
+      return Redirect::to($c->asset_tag, 303)
+                      ->with('computer_inserted', TRUE);
     } else {
       return Redirect::to_action('computers@index', array(), 303)
                       ->with_errors($v)
